0136b4335046e139371341d55013e743c097dc40
William <wdgstl@gmail.com>
2024-03-18 16:16:31 -0400

    created bootstrap.sh

---
+++ mywork/lab5/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/usr/bin/apt update -y
+/usr/bin/apt upgrade -y
+/usr/bin/apt install -y git 
+/usr/bin/apt install -y python3
+/usr/bin/apt install -y apache2
+/usr/bin/apt install -y jq
